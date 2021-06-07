1fab0cd2785889e31bcb78cd7a1d4f00b9574f95
Sandyabhi <abhisandy47@gmail.com>
2021-06-07 21:33:45 +0530

    okk

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/checkAuth");
 
@@ -5,7 +7,7 @@ module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -24,20 +26,37 @@ module.exports = {
         throw new Error(err);
       }
     },
-    Mutation: {
-      async createPost(_, { body }, context) {
-        const user = checkAuth(context);
+  },
 
-        const newPost = new Post({
-          body,
-          user: user.indexOf,
-          username: user.username,
-          createdAt: new Date().toISOString(),
-        });
-        const post = await newPost.save();
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
 
-        return post;
-      },
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+      const post = await newPost.save();
+
+      return post;
+    },
+
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
