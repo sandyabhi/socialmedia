d3396b11ed72f57d0798e8d5e85c81ba1a6b5659
Sandyabhi <abhisandy47@gmail.com>
2021-06-07 15:01:03 +0530

    hlo

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,15 +1,30 @@
 const { gql } = require("apollo-server");
 
-module.exports = gql;
-
-const typeDefs = gql`
+module.exports = gql`
   type Post {
     id: ID!
     body: String!
     createdAt: String!
     username: String!
   }
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    createdAt: String!
+    username: String!
+  }
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
   type Query {
     getPosts: [Post]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
   }
 `;
--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const { Apolloserver, ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
-const Post = require("./model/Post");
-const User = require("./model/User");
 const { MONGODB } = require("./config.js");
+const resolvers = require('./graphql/resolvers')
 
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
