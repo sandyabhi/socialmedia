4d1b496dc6eedccdc1f0dbbb477cee606009f0f3
Sandyabhi <abhisandy47@gmail.com>
2021-06-09 22:23:09 +0530

    client2

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import {Card, Icon, Label} from "semantic-ui-react"
+import React from "react";
+import { Card, Icon, Label } from "semantic-ui-react";
 
-function PostCard({post:{body, craetedAt, id, username, likeCount, commentCount, like}}) {
-    
-    return (
-        <div>
-            
+function PostCard({
+  post: { body, craetedAt, id, username, likeCount, commentCount, like },
+}) {
+  return (
+    <Card>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>Molly Thomas</Card.Header>
+        <Card.Meta>New User</Card.Meta>
+        <Card.Description>
+          Molly wants to add you to the group <strong>musicians</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className="ui two buttons">
+          <Button basic color="green">
+            Approve
+          </Button>
+          <Button basic color="red">
+            Decline
+          </Button>
         </div>
-    )
+      </Card.Content>
+    </Card>
+  );
 }
 
-export default PostCard
+export default PostCard;
