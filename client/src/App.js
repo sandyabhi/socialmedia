de8dc7dc1c3b978b08cd9b8ebced8daef44b3166
Sandyabhi <abhisandy47@gmail.com>
2021-06-15 15:31:04 +0530

    client47

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import { Container } from "semantic-ui-react";
-import MenuBar from "./components/MenuBar";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import { AuthProvider } from "./context/auth";
+import { AuthProvider } from './context/auth';
+import AuthRoute from './util/AuthRoute';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-      <Container>
-        <MenuBar />
-        <Route exact patch="/" component={Home} />
-        <Route exact patch="/login" component={Login} />
-        <Route exact patch="/register" component={Register} />
-      </Container>
-    </Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+
+        </Container>
+      </Router>
     </AuthProvider>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,21 @@
-import React from "react";
-// import { useQuery } from "@apollo/react-hooks";
-// import gql from "graphql-tag";
-import { gql, useQuery } from '@apollo/client';
-
-import { Grid ,Transition} from "semantic-ui-react";
+import React, { useContext } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+//import { gql, useQuery } from '@apollo/client';
+import { AuthContext, AuthProvider } from "../context/auth";
+import { Grid, Transition } from "semantic-ui-react";
 import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 
 function Home() {
+
+  const { user } = useContext(AuthContext)
+
   const {
     loading,
     data: { getPosts: posts } = {}
   } = useQuery(FETCH_POSTS_QUERY)
-  
+
 
   return (
     <Grid columns={3}>
@@ -19,7 +23,11 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
-      
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
